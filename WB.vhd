845affadbd3d714af21c0733c4d5ff0183e996e3
zavgm <zavgmaxden@mail.ru>
2022-12-04 18:13:11 +0300

    wb, classic cycle read

--- WB.vhd
+++ WB.vhd
@@ -13,7 +13,8 @@ entity WB is
 		WB_Sel		: in	std_logic_vector(1 downto 0);
 		WB_STB		: in	std_logic;
 		WB_Cyc		: in	std_logic;
-		WB_Rst		: in	std_logic;
+		WB_Rst		: in	std_logic; -- нет в спецификации у мастера
+		WB_Clk		: in	std_logic; -- нет в спецификации
 		WB_Ack		: out	std_logic;
 		WB_CTI		: in	std_logic_vector(2 downto 0);
 		
@@ -26,30 +27,60 @@ end WB;
 
 architecture Behavioral of WB is
 
-	signal inner_ADC_FTW	: unsigned(31 downto 0);
-	signal inner_clear	: std_logic;
-	signal inner_enable	: std_logic;
+	constant BASE_ADDR	: integer := 16#0100#;
+	constant ADC_FSC_OFF	: integer := 16#0000#;
+	constant ADC_FTW_OFF	: integer := 16#0001#;
+
+	signal reg_ADC_FTW: std_logic_vector(31 downto 0);
+	signal reg_clear	: std_logic;
+	signal reg_enable	: std_logic;
 
 begin
-		
-		process(WB_Cyc)
-		begin
-			if (rising_edge(WB_Cyc)) then
-				if (WB_Rst '0') then'
-					clear <= '0';
-					enable <= '0';
-					ADC_FTW <= (others => '0')
-				elseif ((WB_STB = '1') and (WB_Ack = '1')) then
-					if (WB_Addr = '
-				
-				
-				
-			end if;
-			
-			
-			
-		end process;	
+
+	clear		<= reg_clear;
+	enable	<= reg_enable;
+	ADC_FTW	<= unsigned(reg_ADC_FTW);
 	
+	process(WB_Clk)
+	begin
+		if (rising_edge(WB_Clk)) then
+			if (WB_Rst = '1') then
+				reg_clear <= '0';
+				reg_enable <= '0';
+				reg_ADC_FTW <= (others => '0');
+			elsif ((WB_STB and WB_WE and WB_Cyc) = '1') then
+				for k in 0 to 1 loop -- for each 8-byte-word in WB_DataIn
+					if(WB_Sel(k) = '1') then
+						case to_integer(unsigned(WB_Addr)) + k is
+							when BASE_ADDR + ADC_FSC_OFF => 
+								-- It is freq syn control
+								reg_clear <= WB_DataIn(8*k);
+								reg_enable<= WB_DataIn(8*k+1);
+							when BASE_ADDR + ADC_FTW_OFF =>
+								-- It is 1st part off FTW
+								reg_ADC_FTW(7 downto 0) <= WB_DataIn(8*(k+1)-1 downto 8*k);
+								-- It is 2nd part off FTW
+							when BASE_ADDR + ADC_FTW_OFF + 1 =>
+								reg_ADC_FTW(15 downto 8) <= WB_DataIn(8*(k+1)-1 downto 8*k);
+								-- It is 3rd part off FTW
+							when BASE_ADDR + ADC_FTW_OFF + 2 =>
+								reg_ADC_FTW(23 downto 16) <= WB_DataIn(8*(k+1)-1 downto 8*k);
+								-- It is 4th part off FTW
+							when BASE_ADDR + ADC_FTW_OFF + 3 =>
+								reg_ADC_FTW(31 downto 24) <= WB_DataIn(8*(k+1)-1 downto 8*k);
+							when others => null; -- nothing to do on other addresses
+						end case;
+					end if;
+				end loop;
+			else
+				reg_clear	<= reg_clear;
+				reg_enable	<= reg_enable;
+				reg_ADC_FTW	<= reg_ADC_FTW;
+			end if;
+		end if;
+	end process;
+	
+	WB_Ack <= WB_STB;
 
 end Behavioral;
 
